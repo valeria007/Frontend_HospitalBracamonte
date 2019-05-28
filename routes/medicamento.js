35f2278f0f99b1f250ebd9437147bdbda52bf512
Alvaro Willams Alejandro Arancibia <aljand321@gmail.com>
2019-05-28 15:19:50 -0400

    se aumento la vista medicamento update y se modifico la visa medicamento para que puede mandar datos y recivir datos

--- routes/medicamento.js
+++ routes/medicamento.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const fetch = require('node-fetch');
 
+var url = "http://localhost:3500/api";
+
 var dataGRUPOa;
 router.get('/dataGrupoA',(req,res) =>{
     fetch('http://localhost:3500/api/asignacion')   
@@ -22,16 +24,17 @@ router.get('/dataGrupoA',(req,res) =>{
 
 //serv para renderisar la vista medicamento con datos de la tabla grupo asignacion
 router.get('/medicamentos',(req, res) => {
-    fetch('http://localhost:3500/api/medico')   
+    fetch('http://localhost:3500/api/medicamento')   
         .then(resp => resp.json())
         .then(resp =>{
+            console.log(resp);
             if(dataGRUPOa == null){
-                res.render('medicamentos',{dataGRUPOa})
+                res.render('medicamentos',{dataGRUPOa,resp})
             }else{
                 res.render('medicamentos',{
                     dataGRUPOa,
                     resp
-                })   
+                })  
             }
     })
     .catch(error => {
@@ -56,6 +59,42 @@ router.post('/medicamentos', (req,res) =>{
   .then(data => { 
     res.redirect('/medicamento/dataGrupoA');
   })
+});
+
+//serv para mostrar un solo medicamento
+router.get('/OnlyMedicamento/:id',(req, res) =>{
+    var id = req.params;
+    fetch('http://localhost:3500/api/OnlyMedicamento/'+id.id)   
+        .then(resp => resp.json())
+        .then(resp =>{
+            res.render('medicamentoUPDATE',{
+                resp,
+                dataGRUPOa
+            });
+    })
+    .catch(error => {
+        console.error('Error:', error)
+        res.send("no hay coneccion con el servidor");
+    })
+});
+
+router.post('/updateMedicamento/:id', (req,res) =>{
+    var id = req.params;
+    var data = req.body;
+    var esto = {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers:{
+          'Content-type' : "application/json"
+        }
+    };
+    fetch(url+'/updateMedicamento/'+id.id,esto)
+    .then(res => res.json())
+    .catch(error => console.error('Error:', error))
+    .then(data => { 
+      res.redirect('/medicamento/dataGrupoA');
+    })
+
 });
 
 module.exports = router;
