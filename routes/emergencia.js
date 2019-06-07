e8bc525957bcce0718373d6aa268f44c147a3821
Alvaro Willams Alejandro Arancibia <aljand321@gmail.com>
2019-06-07 02:59:25 -0400

    se midifico cita medica ahora recive el id y no el historial clinico tmb se hizo emrgencia pero falta terminar y muchos detalles

--- routes/emergencia.js
+++ routes/emergencia.js
@@ -6,6 +6,38 @@ router.get('/oneVista', (req, res) => {
     res.render('emergencias/viewDocEnf');
 });
 
-router.get('')
 
+//esta vista muesta la vista principal de emergencia segun doctor o enfermera 
+router.get('/homeEmergencia/:id', (req, res) => {
+    var DogOenf = req.params.id
+    res.render('emergencias/homeEmergencia',{
+        DogOenf //esta variable solo es para saver si es doct@r o enfermer@
+    });
+});
+
+// este serv va servir para traer citas tipo solo emergencia
+router.get('/listEmergencia/:id', (req, res) => {
+    var DogOenf = req.params.id;
+    fetch('http://localhost:3000/api/citas/emergencia')
+        
+        .then(resp => resp.json())
+        .then(resp =>{ 
+            if(resp == ""){                
+                res.render('emergencias/listasEmergencias',{
+                    DogOenf,  //esta variable solo es para saver si es doct@r o enfermer@
+                    resp
+                });
+            }else{
+                res.render('emergencias/listasEmergencias',{
+                    DogOenf,  //esta variable solo es para saver si es doct@r o enfermer@
+                    resp
+                });
+            }
+    })
+    .catch(error => {
+        console.error('Error:', error)
+        res.send("no hay coneccion con el servidor");
+    })
+   
+});
 module.exports = router;
