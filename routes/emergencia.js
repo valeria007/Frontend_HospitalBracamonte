a1aa23ccc0c760a7e557e33a4b14804d6052b89a
Alvaro Willams Alejandro Arancibia <aljand321@gmail.com>
2019-06-10 01:54:43 -0400

    en proceso emergencias falta detalles como enfermera y dar receta y papeleta de internacion

--- routes/emergencia.js
+++ routes/emergencia.js
@@ -17,20 +17,25 @@ router.get('/homeEmergencia/:id', (req, res) => {
 
 // este serv va servir para traer citas tipo solo emergencia
 router.get('/listEmergencia/:id', (req, res) => {
-    var DogOenf = req.params.id;
-    fetch('http://localhost:3000/api/PacienteCita/emergencia')
+    var DogOenf = req.params.id; // esto es para saver si es doctor o enfermera
+    if (dataEmergencia == null){
+        res.redirect('/emergencia/oneVista');
+    }
+    fetch('http://localhost:3000/api/PacienteCita/emergencia') //esta ruta solo trae las citas de tipo emergencia y que el estado sea true
         
         .then(resp => resp.json())
         .then(resp =>{ 
             if(resp == ""){                
                 res.render('emergencias/listasEmergencias',{
                     DogOenf,  //esta variable solo es para saver si es doct@r o enfermer@
-                    resp
+                    resp,
+                    dataEmergencia
                 });
             }else{
                 res.render('emergencias/listasEmergencias',{
                     DogOenf,  //esta variable solo es para saver si es doct@r o enfermer@
-                    resp
+                    resp,
+                    dataEmergencia //mustra las citas ya antendidas
                 });
             }
     })
@@ -39,6 +44,22 @@ router.get('/listEmergencia/:id', (req, res) => {
         res.send("no hay coneccion con el servidor");
     })
    
+});
+
+// ruta para traer citas medicas ya llenadas su formulario
+var dataEmergencia; //esta variable tarea datos las citas que ya fueron llenados su diagnostico
+router.get('/dataEmergencia/:id', (req,res) => {
+    var DogOenf = req.params.id;
+    fetch('http://localhost:3000/api/PacienteCitaFalse/emergencia') //esta ruta solo trae las citas de tipo emergencia y que el estado sea true
+        .then(resp => resp.json())
+        .then(resp =>{ 
+            dataEmergencia = resp;
+            res.redirect('/emergencia/listEmergencia/'+DogOenf)
+    })
+    .catch(error => {
+        console.error('Error:', error)
+        res.send("no hay coneccion con el servidor");
+    })
 });
 
 
@@ -53,7 +74,9 @@ router.get('/listEmergencia/:id', (req, res) => {
 router.get('/RenderConsultaEmergencia', (req,res) => {   
     res.render('emergencias/consultaEmergencia',{
         idHistorial, // em esta variable se encuentran 3 datos id de la cita medica o ficha, historial y tambien si es doctor o enfermera
-        dataPaciente
+        dataPaciente,
+        consultasEmergencia,
+        updateConsultaEmg
     });
 });
 
@@ -73,15 +96,33 @@ router.get('/dataPaciente/:id', (req,res) => {
     
 });
 
+//servicio para sacar las consulta de emergencia del paciente
+var consultasEmergencia;
+router.get('/dataConsultaEmesgencia/:historial', (req, res) => {
+    var historial = req.params.historial 
+    fetch('http://localhost:3000/api/OnlyEmergencia/'+historial)
+    .then(resp => resp.json())
+    .then(resp =>{  
+        consultasEmergencia = resp;    
+        res.redirect('/emergencia/dataPaciente/'+historial) ;            
+    })
+    .catch(error => {
+        console.error('Error:', error)
+        res.send("no hay coneccion con el servidor");
+    })   
+})
+
 var idHistorial; //esta variable es para madar el id de la ficha y el historial del paciente
+var updateConsultaEmg;
 router.get('/GetEmergencia/:historial/:id/:docOenf', (req,res) => {
     var idHistorial1 = req.params;
     idHistorial = idHistorial1
     
     fetch('http://localhost:3000/api/citaEmergencia/'+idHistorial1.id)
         .then(resp => resp.json())
-        .then(resp =>{      
-            res.redirect('/emergencia/dataPaciente/'+idHistorial1.historial) ;            
+        .then(resp =>{
+            updateConsultaEmg = resp;      
+            res.redirect('/emergencia/dataConsultaEmesgencia/'+idHistorial1.historial) ;            
         })
         .catch(error => {
             console.error('Error:', error)
@@ -96,7 +137,7 @@ router.get('/estado/:id', (req,res) => {
     .then(res => res.json())
     .catch(error => console.error('Error:', error))
     .then(data => { 
-      res.redirect('/emergencia/RenderConsultaEmergencia');
+      res.redirect('/emergencia/GetEmergencia/'+idHistorial.historial+'/'+idHistorial.id+'/'+idHistorial.docOenf);
       //res.status(200).send(data)
     })
 
@@ -119,7 +160,46 @@ router.post('/consultaEmergencia/:id', (req,res) => {
     .then(data => { 
       res.redirect('/emergencia/estado/'+id);
     })
+});
+
+/*
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+        Rutas para modificar consulta emergencia de un paciente
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+*/ 
+
+router.post('/updateConEmergencia/:id', (req,res) => {
+    const { id } = req.params;
+    var data = req.body  
+    var esto = {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers:{
+          'Content-type' : "application/json"
+        }
+    };
+    fetch('http://localhost:3000/api/updateEmergencia/'+id,esto)
+    .then(res => res.json())
+    .catch(error => console.error('Error:', error))
+    .then(data => { 
+      res.redirect('/emergencia/GetEmergencia/'+idHistorial.historial+'/'+idHistorial.id+'/'+idHistorial.docOenf);        
+    })  
 })
 
+/*
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+                    Receta De Emergencia
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+*/ 
+
+router.get('/RenderReceta/:id', (req,res) => {
+    res.render('emergencia/recetaEmergencia',{
+
+    })
+});
 
 module.exports = router;
