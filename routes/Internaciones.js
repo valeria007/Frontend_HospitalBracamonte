2ae4fdbf13b4db190bed995b42df75444664e0ad
Alvaro Willams Alejandro Arancibia <aljand321@gmail.com>
2019-06-13 01:12:14 -0400

    se modifico lista hospitalizacion

--- routes/Internaciones.js
+++ routes/Internaciones.js
@@ -16,12 +16,27 @@ router.get('/hospitalizacion',(req, res) => {
 //ser para renderizar lista hospitalizacion mostrando 
 //la lista de ordenes de internacion que se mandan desde consulta o emergencia
 router.get('/ListInternacion', (req,res) => {
-    fetch('http://localhost:3000/api/papeletaInt')
+    fetch('http://localhost:3000/api/PinterTrue') // esta ruta solo trae los datos de tipo true
     .then(resp => resp.json())
     .then(resp =>{
         res.render('hospitalizaciones/listasHospitalizacion',{
-            resp
+            resp,
+            listFalse// lleva la lista de tipo false
         })
+    })        
+    .catch(error => {
+        console.error('Error:', error)
+        res.send("no hay coneccion con el servidor");
+    }) 
+});
+// esta ruta es para traer la lista de internacion de tipo false
+var listFalse;
+router.get('/ListaInternacionF', (req,res) => {
+    fetch('http://localhost:3000/api/PinterFalse') // esta ruta solo trae los datos de tipo true
+    .then(resp => resp.json())
+    .then(resp =>{
+        listFalse = resp;
+        res.redirect('/internaciones/ListInternacion');
     })        
     .catch(error => {
         console.error('Error:', error)
