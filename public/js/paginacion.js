7b87c9ee6aa49806df560b5974b0c7b9a7bfb2b3
valeria007 <sis015abigail@gmail.com>
2019-07-02 20:01:34 -0400

    almacenamiento

--- public/js/paginacion.js
+++ public/js/paginacion.js
@@ -1,141 +1,85 @@
 getPagination('#table-id');
-$('#maxRows').trigger('change');
+					
+		 
 function getPagination (table){
 
-      $('#maxRows').on('change',function(){
-          $('.pagination').html('');						// reset pagination div
-          var trnum = 0 ;									// reset tr counter 
-          var maxRows = parseInt($(this).val());			// get Max Rows from select option
-    
-          var totalRows = $(table+' tbody tr').length;		// numbers of rows 
-         $(table+' tr:gt(0)').each(function(){			// each TR in  table and not the header
-             trnum++;									// Start Counter 
-             if (trnum > maxRows ){						// if tr number gt maxRows
-                 
-                 $(this).hide();							// fade it out 
-             }if (trnum <= maxRows ){$(this).show();}// else fade in Important in case if it ..
-         });											//  was fade out to fade it in 
-         if (totalRows > maxRows){						// if tr total rows gt max rows option
-             var pagenum = Math.ceil(totalRows/maxRows);	// ceil total(rows/maxrows) to get ..  
-                                                         //	numbers of pages 
-             for (var i = 1; i <= pagenum ;){			// for each page append pagination li 
-             $('.pagination').append('<li data-page="'+i+'">\
-                                  <span>'+ i++ +'<span class="sr-only">(current)</span></span>\
-                                </li>').show();
-             }											// end for i 
- 
-     
-        } 												// end if row count > max rows
-        $('.pagination li:first-child').addClass('active'); // add active class to the first li 
-    
-    
-    //SHOWING ROWS NUMBER OUT OF TOTAL DEFAULT
-   showig_rows_count(maxRows, 1, totalRows);
-    //SHOWING ROWS NUMBER OUT OF TOTAL DEFAULT
+          var lastPage = 1 ; 
 
-    $('.pagination li').on('click',function(e){		// on click each page
-    e.preventDefault();
-            var pageNum = $(this).attr('data-page');	// get it's number
-            var trIndex = 0 ;							// reset tr counter
-            $('.pagination li').removeClass('active');	// remove active class from all li 
-            $(this).addClass('active');					// add active class to the clicked 
+    $('#maxRows').on('change',function(evt){
     
-    
-    //SHOWING ROWS NUMBER OUT OF TOTAL
-   showig_rows_count(maxRows, pageNum, totalRows);
-    //SHOWING ROWS NUMBER OUT OF TOTAL
-    
-    
-    
-             $(table+' tr:gt(0)').each(function(){		// each tr in table not the header
-                 trIndex++;								// tr index counter 
-                 // if tr index gt maxRows*pageNum or lt maxRows*pageNum-maxRows fade if out
-                 if (trIndex > (maxRows*pageNum) || trIndex <= ((maxRows*pageNum)-maxRows)){
-                     $(this).hide();		
-                 }else {$(this).show();} 				//else fade in 
-             }); 										// end of for each tr in table
-                });										// end of on click pagination list
-    });
-                                        // end of on select change 
-     
-                            // END OF PAGINATION 
+  lastPage = 1 ; 
+       $('.pagination').find("li").slice(1, -1).remove();
+      var trnum = 0 ;									// reset tr counter 
+      var maxRows = parseInt($(this).val());			// get Max Rows from select option
+
+      if(maxRows == 5000 ){
+
+        $('.pagination').hide();
+      }else {
+        
+        $('.pagination').show();
+      }
+
+      var totalRows = $(table+' tbody tr').length;		// numbers of rows 
+     $(table+' tr:gt(0)').each(function(){			// each TR in  table and not the header
+       trnum++;									// Start Counter 
+       if (trnum > maxRows ){						// if tr number gt maxRows
+         
+         $(this).hide();							// fade it out 
+       }if (trnum <= maxRows ){$(this).show();}// else fade in Important in case if it ..
+     });											//  was fade out to fade it in 
+     if (totalRows > maxRows){						// if tr total rows gt max rows option
+       var pagenum = Math.ceil(totalRows/maxRows);	// ceil total(rows/maxrows) to get ..  
+                             //	numbers of pages 
+       for (var i = 1; i <= pagenum ;){			// for each page append pagination li 
+       $('.pagination #prev').before('<li data-page="'+i+'">\
+                    <span>'+ i++ +'<span class="sr-only">(current)</span></span>\
+                  </li>').show();
+       }											// end for i 
+    } 												// end if row count > max rows
+    $('.pagination [data-page="1"]').addClass('active'); // add active class to the first li 
+    $('.pagination li').on('click',function(evt){		// on click each page
+      evt.stopImmediatePropagation();
+      evt.preventDefault();
+      var pageNum = $(this).attr('data-page');	// get it's number
+
+      var maxRows = parseInt($('#maxRows').val());			// get Max Rows from select option
+
+      if(pageNum == "prev" ){
+        if(lastPage == 1 ){return;}
+        pageNum  = --lastPage ; 
+      }
+      if(pageNum == "next" ){
+        if(lastPage == ($('.pagination li').length -2) ){return;}
+        pageNum  = ++lastPage ; 
+      }
+
+      lastPage = pageNum ;
+      var trIndex = 0 ;							// reset tr counter
+      $('.pagination li').removeClass('active');	// remove active class from all li 
+      $('.pagination [data-page="'+lastPage+'"]').addClass('active');
+       $(table+' tr:gt(0)').each(function(){		// each tr in table not the header
+         trIndex++;								// tr index counter 
+         // if tr index gt maxRows*pageNum or lt maxRows*pageNum-maxRows fade if out
+         if (trIndex > (maxRows*pageNum) || trIndex <= ((maxRows*pageNum)-maxRows)){
+           $(this).hide();		
+         }else {$(this).show();} 				//else fade in 
+       }); 										// end of for each tr in table
+        });										// end of on click pagination list
+
+  }).val(5).change();
 
 }	
 
 
-        
-
-// SI SETTING
 $(function(){
 // Just to append id number for each row  
-default_index();
-                
-});
+        $('table tr:eq(0)').prepend('<th> ID </th>')
 
-//ROWS SHOWING FUNCTION
-function showig_rows_count(maxRows, pageNum, totalRows) {
-//Default rows showing
-    var end_index = maxRows*pageNum;
-    var start_index = ((maxRows*pageNum)- maxRows) + parseFloat(1);
-    var string = 'Showing '+ start_index + ' to ' + end_index +' of ' + totalRows + ' entries';               
-    $('.rows_count').html(string);
-}
+        var id = 0;
 
-// CREATING INDEX
-function default_index() {
-$('table tr:eq(0)').prepend('<th> ID </th>')
-
-                var id = 0;
-
-                $('table tr:gt(0)').each(function(){	
-                    id++
-                    $(this).prepend('<td>'+id+'</td>');
-                });
-}
-
-// All Table search script
-function FilterkeyWord_all_table() {
-
-// Count td if you want to search on all table instead of specific column
-
-var count = $('.table').children('tbody').children('tr:first-child').children('td').length; 
-
-    // Declare variables
-var input, filter, table, tr, td, i;
-input = document.getElementById("search_input_all");
-var input_value =     document.getElementById("search_input_all").value;
-    filter = input.value.toLowerCase();
-if(input_value !=''){
-    table = document.getElementById("table-id");
-    tr = table.getElementsByTagName("tr");
-
-    // Loop through all table rows, and hide those who don't match the search query
-    for (i = 1; i < tr.length; i++) {
-      
-      var flag = 0;
-       
-      for(j = 0; j < count; j++){
-        td = tr[i].getElementsByTagName("td")[j];
-        if (td) {
-         
-            var td_text = td.innerHTML;  
-            if (td.innerHTML.toLowerCase().indexOf(filter) > -1) {
-            //var td_text = td.innerHTML;  
-            //td.innerHTML = 'shaban';
-              flag = 1;
-            } else {
-              //DO NOTHING
-            }
-          }
-        }
-      if(flag==1){
-                 tr[i].style.display = "";
-      }else {
-         tr[i].style.display = "none";
-      }
-    }
-}else {
-  //RESET TABLE
-  $('#maxRows').trigger('change');
-}
-}
+        $('table tr:gt(0)').each(function(){	
+          id++
+          $(this).prepend('<td>'+id+'</td>');
+        });
+})
