d907df2a6d8ce3d61888792cf96136ed0eea1bcb
MaritzaCho <lunita.ma.123@gmail.com>
2019-08-13 15:23:26 -0400

    citas fichas

--- public/plugins/data-tables/dynamic_table_init.js
+++ public/plugins/data-tables/dynamic_table_init.js
@@ -2,9 +2,9 @@ function fnFormatDetails ( oTable, nTr )
 {
     var aData = oTable.fnGetData( nTr );
     var sOut = '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">';
-    sOut += '<tr><td>Rendering engine:</td><td>'+aData[1]+' '+aData[4]+'</td></tr>';
-    sOut += '<tr><td>Link to source:</td><td>Could provide a link here</td></tr>';
-    sOut += '<tr><td>Extra info:</td><td>And any further details here (images etc)</td></tr>';
+    sOut += '<tr><td>Número de Historial:</td><td>'+aData[2]+' </td></tr>';
+    sOut += '<tr><td>Nombre Completo:</td><td>'+aData[3]+' '+aData[4]+'</td></tr>';
+    sOut += '<tr><td>CI.:</td><td>'+aData[5]+'</td></tr>';
     sOut += '</table>';
 
     return sOut;
@@ -51,13 +51,13 @@ $(document).ready(function() {
         if ( oTable.fnIsOpen(nTr) )
         {
             /* This row is already open - close it */
-            this.src = "plugins/advanced-datatable/images/details_open.png";
+            this.src = "/plugins/advanced-datatable/images/details_open.png";
             oTable.fnClose( nTr );
         }
         else
         {
             /* Open this row */
-            this.src = "plugins/advanced-datatable/images/details_close.png";
+            this.src = "/plugins/advanced-datatable/images/details_close.png";
             oTable.fnOpen( nTr, fnFormatDetails(oTable, nTr), 'details' );
         }
     } );
