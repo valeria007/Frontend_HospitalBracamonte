4444f1dea8fd06495aa6737485d48efe36946216
MaritzaCho <lunita.ma.123@gmail.com>
2019-08-17 17:09:32 -0400

    vista doctor

--- public/plugins/data-tables/dynamic_table_init2.js
+++ public/plugins/data-tables/dynamic_table_init2.js
@@ -1,10 +1,11 @@
+
 function fnFormatDetails ( oTable, nTr )
 {
     var aData = oTable.fnGetData( nTr );
     var sOut = '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">';
-    sOut += '<tr><td>Numero de Historial:</td><td>'+aData[2]+' '+aData[3]+'</td></tr>';
-    sOut += '<tr><td>Link to source:</td><td>Could provide a link here</td></tr>';
-    sOut += '<tr><td>Extra info:</td><td>And any further details here (images etc)</td></tr>';
+    sOut += '<tr><td>Número de Historial:</td><td>'+aData[2]+' </td></tr>';
+    sOut += '<tr><td>Nombre Completo:</td><td>'+aData[3]+' '+aData[4]+'</td></tr>';
+    sOut += '<tr><td>CI.:</td><td>'+aData[5]+'</td></tr>';
     sOut += '</table>';
 
     return sOut;
