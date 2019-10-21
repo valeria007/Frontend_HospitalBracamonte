07e3ab10282115a4856a93d041ed5ab394304915
MaritzaCho <lunita.ma.123@gmail.com>
2019-10-21 00:28:07 -0400

    alerta2

--- public/url/export.js
+++ public/url/export.js
@@ -1,7 +1,3 @@
 var data_url = {
-<<<<<<< HEAD
     url_front_end: 'http://192.168.1.16:7000'
-=======
-    url_front_end: 'http://192.168.1.7:7000'
->>>>>>> e20f6e9bd762c4cdd2b863ca97d6f069bf45c096
 }
